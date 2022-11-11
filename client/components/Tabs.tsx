5124665eaa3d287b61069f3626365aea564db496
Alexander <alexanderholmberg91@gmail.com>
2022-11-11 21:01:27 +0100

    add defaults

---
+++ client/components/Tabs.tsx
@@ -0,0 +1,36 @@
+const tabs = [
+  { name: 'My Account', href: '#', current: false },
+  { name: 'Company', href: '#', current: false },
+  { name: 'Team Members', href: '#', current: true },
+  { name: 'Billing', href: '#', current: false },
+];
+
+function classNames(...classes: any[]) {
+  return classes.filter(Boolean).join(' ');
+}
+
+export default function Tabs() {
+  return (
+    <div>
+      <div className="hidden sm:block">
+        <nav className="flex space-x-4" aria-label="Tabs">
+          {tabs.map((tab) => (
+            <a
+              key={tab.name}
+              href={tab.href}
+              className={classNames(
+                tab.current
+                  ? 'bg-gray-100 text-gray-700'
+                  : 'text-gray-500 hover:text-gray-700',
+                'px-3 py-2 font-medium text-sm rounded-md'
+              )}
+              aria-current={tab.current ? 'page' : undefined}
+            >
+              {tab.name}
+            </a>
+          ))}
+        </nav>
+      </div>
+    </div>
+  );
+}
